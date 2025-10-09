58db76341115fd7fd2dd4d83c061a3ccffcb2582
Me Lol <szabo-benedek@mailbox.unideb.hu>
2025-10-09 15:16:58 +0200

    codefix made during class

--- data/basic/generator.py
+++ data/basic/generator.py
@@ -3,18 +3,30 @@ from random import random
 from random import choice
 import string
 from data.basic.model_classes import Player
-
+#region id generator
 def generate_id() -> str:
     chars = string.ascii_letters + string.digits
     return ''.join([choice(chars) for _ in range(6)])
-def generate_players(n: int) -> list[Player]:
-    fake = Faker()
+#endregion
+#region player generator
+def generate_Players(howmuch: int = 1, locale: str = "en_US") -> list[Player]:
+    fake = Faker(locale)
+    ret_Players = []
+    for i in range(howmuch):
+        ret_Players.append(Player(generate_id(), fake.name(), fake.country()))
+    return ret_Players
+#endregion
+#region test
+if __name__ == '__main__':
+    n = 100
+    mylist = generate_Players(n)
     for i in range(n):
-        return [Player(
-            "P-" + generate_id(),
-            fake.name(),
-            fake.country())]
-if __name__ == "__main__":
-    i = 1
-    for i in range(4):
-        print(generate_players(i))
+        print(mylist[i])
+        with open("player_list.txt", "w") as players_file:
+            n = 100
+            mylist = generate_Players(n)
+            for i in range(n):
+                if i == 99:
+                    players_file.write(str(mylist[i]))
+                players_file.write("{0}\n".format(str(mylist[i])))
+#endregion
